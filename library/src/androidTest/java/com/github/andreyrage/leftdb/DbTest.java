5326206775b5614edc5a09b7bbb3f163d1ccd915
Andrew Horishnii <andrii.horishnii@ekreative.com>
2015-11-18 17:28:24 +0200

    add tests

---
+++ library/src/androidTest/java/com/github/andreyrage/leftdb/DbTest.java
@@ -0,0 +1,237 @@
+package com.github.andreyrage.leftdb;
+
+import android.test.AndroidTestCase;
+
+import com.github.andreyrage.leftdb.entities.AllFields;
+import com.github.andreyrage.leftdb.entities.ChildMany;
+import com.github.andreyrage.leftdb.entities.ChildOne;
+import com.github.andreyrage.leftdb.entities.ParentMany;
+import com.github.andreyrage.leftdb.entities.ParentOne;
+import com.github.andreyrage.leftdb.entities.SerializableObject;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+public class DbTest extends AndroidTestCase {
+
+	private DBUtils dbUtils;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		dbUtils = DBUtils.newInstance(getContext(), "test.sqlite", 1);
+		assertNotNull(dbUtils.db);
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		dbUtils.getDbHandler().deleteDataBase();
+	}
+
+	public void testAddEntity() throws Exception {
+		SerializableObject child = new SerializableObject(1, "child", null);
+		SerializableObject parent = new SerializableObject(1, "parrent", child);
+		ArrayList<SerializableObject> list = new ArrayList<>();
+		list.add(child);
+		list.add(parent);
+		AllFields allFields = new AllFields(
+				1,
+				(short) 10,
+				(short) 10,
+				20,
+				20,
+				(long) 30,
+				(long) 30,
+				0.40f,
+				0.40f,
+				0.50d,
+				0.50d,
+				true,
+				true,
+				"simple string",
+				new BigDecimal(1345892734),
+				new Date(System.currentTimeMillis()),
+				Calendar.getInstance(),
+				parent,
+				parent,
+				list
+		);
+
+		dbUtils.add(allFields);
+		List<AllFields> dbList = dbUtils.getAll(AllFields.class);
+
+		assertEquals(1, dbList.size());
+		assertEquals(allFields, dbList.get(0));
+	}
+
+	public void testAddEntityWithNull() throws Exception {
+		AllFields allFields = new AllFields(
+				1,
+				(short) 10,
+				null,
+				20,
+				null,
+				(long) 30,
+				null,
+				0.40f,
+				null,
+				0.50d,
+				null,
+				true,
+				null,
+				null,
+				null,
+				null,
+				null,
+				null,
+				null,
+				null
+		);
+
+		dbUtils.add(allFields);
+		List<AllFields> dbList = dbUtils.getAll(AllFields.class);
+
+		assertEquals(1, dbList.size());
+		assertEquals(allFields, dbList.get(0));
+	}
+
+	public void testAddEntityAutoInc() throws Exception {
+		SerializableObject object = new SerializableObject();
+
+		dbUtils.add(object);
+		dbUtils.add(object);
+		List<SerializableObject> dbList = dbUtils.getAll(SerializableObject.class);
+
+		assertEquals(2, dbList.size());
+		assertEquals(1, dbList.get(0).getId());
+		assertEquals(2, dbList.get(1).getId());
+	}
+
+	public void testColumnName() throws Exception {
+		SerializableObject object = new SerializableObject(100, "simple name", null);
+
+		dbUtils.add(object);
+		List<SerializableObject> dbList = dbUtils.getAll(SerializableObject.class);
+
+		assertEquals(1, dbList.size());
+		assertEquals("simple name", dbList.get(0).getName());
+	}
+
+	public void testColumnIgnore() throws Exception {
+		SerializableObject object = new SerializableObject(100, "simple name", new SerializableObject());
+
+		dbUtils.add(object);
+		List<SerializableObject> dbList = dbUtils.getAll(SerializableObject.class);
+
+		assertEquals(1, dbList.size());
+		assertNotSame(object, dbList.get(0));
+		assertEquals("simple name", dbList.get(0).getName());
+		assertNull(dbList.get(0).getObject());
+	}
+
+	public void testAddList() throws Exception {
+		SerializableObject object1 = new SerializableObject(100, "simple name", new SerializableObject());
+		SerializableObject object2 = new SerializableObject(101, "simple name", new SerializableObject());
+
+		List<SerializableObject> list = new ArrayList<>();
+		list.add(object1);
+		list.add(object2);
+
+		dbUtils.add(list);
+		List<SerializableObject> dbList = dbUtils.getAll(SerializableObject.class);
+
+		assertEquals(2, dbList.size());
+	}
+
+	public void testGetAllWhere() throws Exception {
+		SerializableObject object1 = new SerializableObject(100, "simple name", null);
+		SerializableObject object2 = new SerializableObject(101, "simple name", null);
+
+		List<SerializableObject> list = new ArrayList<>();
+		list.add(object1);
+		list.add(object2);
+
+		dbUtils.add(list);
+		List<SerializableObject> dbList = dbUtils.getAllWhere("id = 101", SerializableObject.class);
+
+		assertEquals(1, dbList.size());
+		assertEquals(object2, dbList.get(0));
+	}
+
+	public void testDeleteAll() throws Exception {
+		SerializableObject object1 = new SerializableObject(100, "simple name", null);
+		SerializableObject object2 = new SerializableObject(101, "simple name", null);
+
+		List<SerializableObject> list = new ArrayList<>();
+		list.add(object1);
+		list.add(object2);
+
+		dbUtils.add(list);
+		dbUtils.deleteAll(SerializableObject.class);
+		List<SerializableObject> dbList = dbUtils.getAll(SerializableObject.class);
+
+		assertTrue(dbList.isEmpty());
+	}
+
+	public void testDeleteWhere() throws Exception {
+		SerializableObject object1 = new SerializableObject(100, "simple name", null);
+		SerializableObject object2 = new SerializableObject(101, "simple name", null);
+
+		List<SerializableObject> list = new ArrayList<>();
+		list.add(object1);
+		list.add(object2);
+
+		dbUtils.add(list);
+		dbUtils.deleteWhere(SerializableObject.class, "id = 100");
+		List<SerializableObject> dbList = dbUtils.getAll(SerializableObject.class);
+
+		assertEquals(1, dbList.size());
+		assertEquals(object2, dbList.get(0));
+	}
+
+	public void testOneToOne() throws Exception {
+		ParentOne parentOne1 = new ParentOne(200, new ChildOne("child1"));
+		ParentOne parentOne2 = new ParentOne(201, new ChildOne("child2"));
+		List<ParentOne> list = new ArrayList<>();
+		list.add(parentOne1);
+		list.add(parentOne2);
+
+		dbUtils.add(list);
+		List<ParentOne> dbList = dbUtils.getAll(ParentOne.class);
+
+		assertEquals(2, dbList.size());
+		assertEquals("child1", dbList.get(0).getChild().getName());
+		assertEquals("child2", dbList.get(1).getChild().getName());
+	}
+
+	public void testOneToMany() throws Exception {
+		List<ChildMany> childList1 = new ArrayList<>();
+		childList1.add(new ChildMany("child1"));
+		childList1.add(new ChildMany("child2"));
+		ParentMany parentMany1 = new ParentMany(200, childList1);
+		List<ChildMany> childList2 = new ArrayList<>();
+		childList2.add(new ChildMany("child3"));
+		childList2.add(new ChildMany("child4"));
+		childList2.add(new ChildMany("child5"));
+		ParentMany parentMany2 = new ParentMany(201, childList2);
+		List<ParentMany> list = new ArrayList<>();
+		list.add(parentMany1);
+		list.add(parentMany2);
+
+		dbUtils.add(list);
+		List<ParentMany> dbList = dbUtils.getAll(ParentMany.class);
+
+		assertEquals(2, dbList.size());
+		assertEquals(2, dbList.get(0).getChilds().size());
+		assertEquals("child1", dbList.get(0).getChilds().get(0).getName());
+		assertEquals("child2", dbList.get(0).getChilds().get(1).getName());
+		assertEquals(3, dbList.get(1).getChilds().size());
+		assertEquals("child3", dbList.get(1).getChilds().get(0).getName());
+		assertEquals("child4", dbList.get(1).getChilds().get(1).getName());
+		assertEquals("child5", dbList.get(1).getChilds().get(2).getName());
+	}
+}
---
+++ library/src/androidTest/java/com/github/andreyrage/leftdb/entities/AllFields.java
@@ -0,0 +1,315 @@
+package com.github.andreyrage.leftdb.entities;
+
+import com.github.andreyrage.leftdb.ColumnAutoInc;
+import com.github.andreyrage.leftdb.ColumnDAO;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by rage on 11/18/15.
+ */
+public class AllFields {
+
+    @ColumnAutoInc private long id;
+    private short shortP;
+    private Short shortW;
+    private int integerP;
+    private Integer integerW;
+    private long longP;
+    private Long longW;
+    private float floatP;
+    private Float floatW;
+    private double doubleP;
+    private Double doubleW;
+    private boolean booleanP;
+    private Boolean booleanW;
+    private String stringO;
+    private BigDecimal bigDecimalO;
+    private Date dateO;
+    private Calendar calendarO;
+    @ColumnDAO private SerializableObject dao;
+    private SerializableObject serialize;
+    private ArrayList<SerializableObject> list;
+
+    public AllFields() {
+    }
+
+    public AllFields(long id, short shortP, Short shortW, int integerP, Integer integerW, long longP, Long longW, float floatP, Float floatW, double doubleP, Double doubleW, boolean booleanP, Boolean booleanW, String stringO, BigDecimal bigDecimalO, Date dateO, Calendar calendarO, SerializableObject dao, SerializableObject serialize, ArrayList<SerializableObject> list) {
+        this.id = id;
+        this.shortP = shortP;
+        this.shortW = shortW;
+        this.integerP = integerP;
+        this.integerW = integerW;
+        this.longP = longP;
+        this.longW = longW;
+        this.floatP = floatP;
+        this.floatW = floatW;
+        this.doubleP = doubleP;
+        this.doubleW = doubleW;
+        this.booleanP = booleanP;
+        this.booleanW = booleanW;
+        this.stringO = stringO;
+        this.bigDecimalO = bigDecimalO;
+        this.dateO = dateO;
+        this.calendarO = calendarO;
+        this.dao = dao;
+        this.serialize = serialize;
+        this.list = list;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AllFields allFields = (AllFields) o;
+
+        if (id != allFields.id) return false;
+        if (shortP != allFields.shortP) return false;
+        if (integerP != allFields.integerP) return false;
+        if (longP != allFields.longP) return false;
+        if (Float.compare(allFields.floatP, floatP) != 0) return false;
+        if (Double.compare(allFields.doubleP, doubleP) != 0) return false;
+        if (booleanP != allFields.booleanP) return false;
+        if (shortW != null ? !shortW.equals(allFields.shortW) : allFields.shortW != null)
+            return false;
+        if (integerW != null ? !integerW.equals(allFields.integerW) : allFields.integerW != null)
+            return false;
+        if (longW != null ? !longW.equals(allFields.longW) : allFields.longW != null) return false;
+        if (floatW != null ? !floatW.equals(allFields.floatW) : allFields.floatW != null)
+            return false;
+        if (doubleW != null ? !doubleW.equals(allFields.doubleW) : allFields.doubleW != null)
+            return false;
+        if (booleanW != null ? !booleanW.equals(allFields.booleanW) : allFields.booleanW != null)
+            return false;
+        if (stringO != null ? !stringO.equals(allFields.stringO) : allFields.stringO != null)
+            return false;
+        if (bigDecimalO != null ? !bigDecimalO.equals(allFields.bigDecimalO) : allFields.bigDecimalO != null)
+            return false;
+        if (dateO != null ? !dateO.equals(allFields.dateO) : allFields.dateO != null) return false;
+        if (calendarO != null ? !calendarO.equals(allFields.calendarO) : allFields.calendarO != null)
+            return false;
+        if (dao != null ? !dao.equals(allFields.dao) : allFields.dao != null) return false;
+        if (serialize != null ? !serialize.equals(allFields.serialize) : allFields.serialize != null)
+            return false;
+        return !(list != null ? !list.equals(allFields.list) : allFields.list != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (int) shortP;
+        result = 31 * result + (shortW != null ? shortW.hashCode() : 0);
+        result = 31 * result + integerP;
+        result = 31 * result + (integerW != null ? integerW.hashCode() : 0);
+        result = 31 * result + (int) (longP ^ (longP >>> 32));
+        result = 31 * result + (longW != null ? longW.hashCode() : 0);
+        result = 31 * result + (floatP != +0.0f ? Float.floatToIntBits(floatP) : 0);
+        result = 31 * result + (floatW != null ? floatW.hashCode() : 0);
+        temp = Double.doubleToLongBits(doubleP);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (doubleW != null ? doubleW.hashCode() : 0);
+        result = 31 * result + (booleanP ? 1 : 0);
+        result = 31 * result + (booleanW != null ? booleanW.hashCode() : 0);
+        result = 31 * result + (stringO != null ? stringO.hashCode() : 0);
+        result = 31 * result + (bigDecimalO != null ? bigDecimalO.hashCode() : 0);
+        result = 31 * result + (dateO != null ? dateO.hashCode() : 0);
+        result = 31 * result + (calendarO != null ? calendarO.hashCode() : 0);
+        result = 31 * result + (dao != null ? dao.hashCode() : 0);
+        result = 31 * result + (serialize != null ? serialize.hashCode() : 0);
+        result = 31 * result + (list != null ? list.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "AllFields{" +
+                "id=" + id +
+                ", shortP=" + shortP +
+                ", shortW=" + shortW +
+                ", integerP=" + integerP +
+                ", integerW=" + integerW +
+                ", longP=" + longP +
+                ", longW=" + longW +
+                ", floatP=" + floatP +
+                ", floatW=" + floatW +
+                ", doubleP=" + doubleP +
+                ", doubleW=" + doubleW +
+                ", booleanP=" + booleanP +
+                ", booleanW=" + booleanW +
+                ", stringO='" + stringO + '\'' +
+                ", bigDecimalO=" + bigDecimalO +
+                ", dateO=" + dateO +
+                ", calendarO=" + calendarO +
+                ", dao=" + dao +
+                ", serialize=" + serialize +
+                ", list=" + list +
+                '}';
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public short getShortP() {
+        return shortP;
+    }
+
+    public void setShortP(short shortP) {
+        this.shortP = shortP;
+    }
+
+    public Short getShortW() {
+        return shortW;
+    }
+
+    public void setShortW(Short shortW) {
+        this.shortW = shortW;
+    }
+
+    public int getIntegerP() {
+        return integerP;
+    }
+
+    public void setIntegerP(int integerP) {
+        this.integerP = integerP;
+    }
+
+    public Integer getIntegerW() {
+        return integerW;
+    }
+
+    public void setIntegerW(Integer integerW) {
+        this.integerW = integerW;
+    }
+
+    public long getLongP() {
+        return longP;
+    }
+
+    public void setLongP(long longP) {
+        this.longP = longP;
+    }
+
+    public Long getLongW() {
+        return longW;
+    }
+
+    public void setLongW(Long longW) {
+        this.longW = longW;
+    }
+
+    public float getFloatP() {
+        return floatP;
+    }
+
+    public void setFloatP(float floatP) {
+        this.floatP = floatP;
+    }
+
+    public Float getFloatW() {
+        return floatW;
+    }
+
+    public void setFloatW(Float floatW) {
+        this.floatW = floatW;
+    }
+
+    public double getDoubleP() {
+        return doubleP;
+    }
+
+    public void setDoubleP(double doubleP) {
+        this.doubleP = doubleP;
+    }
+
+    public Double getDoubleW() {
+        return doubleW;
+    }
+
+    public void setDoubleW(Double doubleW) {
+        this.doubleW = doubleW;
+    }
+
+    public boolean isBooleanP() {
+        return booleanP;
+    }
+
+    public void setBooleanP(boolean booleanP) {
+        this.booleanP = booleanP;
+    }
+
+    public Boolean getBooleanW() {
+        return booleanW;
+    }
+
+    public void setBooleanW(Boolean booleanW) {
+        this.booleanW = booleanW;
+    }
+
+    public String getStringO() {
+        return stringO;
+    }
+
+    public void setStringO(String stringO) {
+        this.stringO = stringO;
+    }
+
+    public BigDecimal getBigDecimalO() {
+        return bigDecimalO;
+    }
+
+    public void setBigDecimalO(BigDecimal bigDecimalO) {
+        this.bigDecimalO = bigDecimalO;
+    }
+
+    public Date getDateO() {
+        return dateO;
+    }
+
+    public void setDateO(Date dateO) {
+        this.dateO = dateO;
+    }
+
+    public Calendar getCalendarO() {
+        return calendarO;
+    }
+
+    public void setCalendarO(Calendar calendarO) {
+        this.calendarO = calendarO;
+    }
+
+    public SerializableObject getDao() {
+        return dao;
+    }
+
+    public void setDao(SerializableObject dao) {
+        this.dao = dao;
+    }
+
+    public SerializableObject getSerialize() {
+        return serialize;
+    }
+
+    public void setSerialize(SerializableObject serialize) {
+        this.serialize = serialize;
+    }
+
+    public ArrayList<SerializableObject> getList() {
+        return list;
+    }
+
+    public void setList(ArrayList<SerializableObject> list) {
+        this.list = list;
+    }
+}
