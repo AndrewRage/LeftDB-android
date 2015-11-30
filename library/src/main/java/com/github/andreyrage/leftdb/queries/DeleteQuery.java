4ccff8a4a7b1c0c38f35373d6a312c9b0ec6fb2a
Vlad Vysotsky <vlad.vysotskyi@ekreative.com>
2015-11-30 10:20:00 +0200

    Simplified queries

--- library/src/main/java/com/github/andreyrage/leftdb/queries/DeleteQuery.java
+++ library/src/main/java/com/github/andreyrage/leftdb/queries/DeleteQuery.java
@@ -4,133 +4,128 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import com.github.andreyrage.leftdb.annotation.TableName;
-import com.github.andreyrage.leftdb.interfaces.Func;
 
 import java.util.List;
 
-import static com.github.andreyrage.leftdb.utils.CheckNullUtils.*;
+import static com.github.andreyrage.leftdb.utils.CheckNullUtils.checkNotNull;
+import static com.github.andreyrage.leftdb.utils.CheckNullUtils.nonNullString;
+import static com.github.andreyrage.leftdb.utils.CheckNullUtils.unmodifiableListOfStrings;
 
 /**
  * Created by Vlad on 11/20/15.
  */
 public final class DeleteQuery {
 
-    @NonNull private final Class<?> entity;
-    @NonNull private final String where;
-    @NonNull private final List<String> whereArgs;
+	@NonNull private final Class<?> entity;
+	@NonNull private final String where;
+	@NonNull private final List<String> whereArgs;
 
-    private DeleteQuery(@NonNull Class<?> entity, @NonNull String where, @NonNull List<String> whereArgs) {
-        this.entity = entity;
-        this.where = where;
-        this.whereArgs = whereArgs;
-    }
+	private DeleteQuery(@NonNull Class<?> entity, @NonNull String where,
+						@NonNull List<String> whereArgs) {
+		this.entity = entity;
+		this.where = where;
+		this.whereArgs = whereArgs;
+	}
 
-    @NonNull
-    public String table() {
-        if (entity.isAnnotationPresent(TableName.class)) {
-            return entity.getAnnotation(TableName.class).value();
-        }
-        return entity.getSimpleName();
-    }
+	@NonNull
+	public String table() {
+		if (entity.isAnnotationPresent(TableName.class)) {
+			return entity.getAnnotation(TableName.class).value();
+		}
+		return entity.getSimpleName();
+	}
 
-    @NonNull
-    Class<?> entity() {
-        return entity;
-    }
+	@NonNull Class<?> entity() {
+		return entity;
+	}
 
-    @NonNull
-    public String where() {
-        return where;
-    }
+	@NonNull
+	public String where() {
+		return where;
+	}
 
-    @NonNull
-    public List<String> whereArgs() {
-        return whereArgs;
-    }
+	@NonNull
+	public List<String> whereArgs() {
+		return whereArgs;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
 
-        DeleteQuery that = (DeleteQuery) o;
+		DeleteQuery that = (DeleteQuery) o;
 
-        return entity.equals(that.entity)
-                && where.equals(that.where)
-                && whereArgs.equals(that.whereArgs);
-    }
+		return entity.equals(that.entity)
+				&& where.equals(that.where)
+				&& whereArgs.equals(that.whereArgs);
+	}
 
-    @Override
-    public int hashCode() {
-        int result = entity.hashCode();
-        result = 31 * result + where.hashCode();
-        result = 31 * result + whereArgs.hashCode();
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		int result = entity.hashCode();
+		result = 31 * result + where.hashCode();
+		result = 31 * result + whereArgs.hashCode();
+		return result;
+	}
 
-    @Override
-    public String toString() {
-        return "DeleteQuery{" +
-                "entity='" + entity.getSimpleName() + '\'' +
-                ", where='" + where + '\'' +
-                ", whereArgs=" + whereArgs +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "DeleteQuery{" +
+				"entity='" + entity.getSimpleName() + '\'' +
+				", where='" + where + '\'' +
+				", whereArgs=" + whereArgs +
+				'}';
+	}
 
-    @NonNull
-    public static Builder builder() {
-        return new Builder();
-    }
+	@NonNull
+	public static Builder builder() {
+		return new Builder();
+	}
 
-    public static final class Builder {
+	public static final class Builder {
 
-        private Class<?> entity;
+		private Class<?> entity;
 
-        private String where;
+		private String where;
 
-        private List<String> whereArgs;
+		private List<String> whereArgs;
 
-        Builder() {
-        }
+		Builder() {
+		}
 
-        @NonNull
-        public Builder entity(@NonNull Class<?> entity) {
-            checkNotNull(entity, "Table name is null or empty");
-            this.entity = entity;
-            return this;
-        }
+		@NonNull
+		public Builder entity(@NonNull Class<?> entity) {
+			checkNotNull(entity, "Table name is null or empty");
+			this.entity = entity;
+			return this;
+		}
 
-        @NonNull
-        public Builder where(@Nullable String where) {
-            this.where = nonNullString(where);
-            return this;
-        }
+		@NonNull
+		public Builder where(@Nullable String where) {
+			this.where = nonNullString(where);
+			return this;
+		}
 
-        @NonNull
-        public Builder whereArgs(@Nullable Object... whereArgs) {
-            this.whereArgs = unmodifiableListOf(String.class, new Func<List<String>, Object, Void>() {
-                @Override
-                public Void invoke(List<String> strings, Object o) {
-                    strings.add(o != null ? o.toString() : "null");
-                    return null;
-                }
-            }, whereArgs);
-            return this;
-        }
+		@NonNull
+		public Builder whereArgs(@Nullable Object... whereArgs) {
+			this.whereArgs = unmodifiableListOfStrings(whereArgs);
+			return this;
+		}
 
-        @NonNull
-        public DeleteQuery build() {
-            if (where == null && whereArgs != null && !whereArgs.isEmpty()) {
-                throw new IllegalStateException("You can not use whereArgs without where clause");
-            }
+		@NonNull
+		public DeleteQuery build() {
+			if (where == null && whereArgs != null && !whereArgs.isEmpty()) {
+				throw new IllegalStateException("You can not use whereArgs without where clause");
+			}
 
-            return new DeleteQuery(
-                    entity,
-                    where,
-                    whereArgs
-            );
-        }
+			return new DeleteQuery(
+					entity,
+					where,
+					whereArgs
+			);
+		}
 
-    }
+	}
 
 }
--- library/src/main/java/com/github/andreyrage/leftdb/utils/CheckNullUtils.java
+++ library/src/main/java/com/github/andreyrage/leftdb/utils/CheckNullUtils.java
@@ -4,8 +4,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 
-import com.github.andreyrage.leftdb.interfaces.Func;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -15,41 +13,41 @@ import java.util.List;
  */
 public class CheckNullUtils {
 
-    public static void checkNotNull(@Nullable Object object, @NonNull String message) {
-        if (object == null) {
-            throw new NullPointerException(message);
-        }
-    }
+	public static void checkNotNull(@Nullable Object object, @NonNull String message) {
+		if (object == null) {
+			throw new NullPointerException(message);
+		}
+	}
 
-    @SafeVarargs
-    @NonNull
-    public static <T, A> List<T> unmodifiableListOf(Class<T> clazz, Func<List<T>, A, Void> func, @Nullable A... args) {
-        if (args == null || args.length == 0) {
-            return Collections.emptyList();
-        } else {
-            final List<T> list = new ArrayList<>(args.length);
-            for (A arg : args) {
-                func.invoke(list, arg);
-            }
-            return Collections.unmodifiableList(list);
-        }
-    }
+	@SafeVarargs
+	@NonNull
+	public static <A> List<String> unmodifiableListOfStrings(@Nullable A... args) {
+		if (args == null || args.length == 0) {
+			return Collections.emptyList();
+		} else {
+			final List<String> list = new ArrayList<>(args.length);
+			for (A arg : args) {
+				list.add((arg != null ? arg.toString() : "null"));
+			}
+			return Collections.unmodifiableList(list);
+		}
+	}
 
-    @Nullable
-    public static String nullableString(@Nullable String str) {
-        return TextUtils.isEmpty(str) ? null : str;
-    }
+	@Nullable
+	public static String nullableString(@Nullable String str) {
+		return TextUtils.isEmpty(str) ? null : str;
+	}
 
-    @Nullable
-    public static String[] nullableArrayOfStrings(@Nullable List<String> list) {
-        return list == null || list.isEmpty()
-                ? null
-                : list.toArray(new String[list.size()]);
-    }
+	@Nullable
+	public static String[] nullableArrayOfStrings(@Nullable List<String> list) {
+		return list == null || list.isEmpty()
+				? null
+				: list.toArray(new String[list.size()]);
+	}
 
-    @NonNull
-    public static String nonNullString(@Nullable String str) {
-        return str == null ? "" : str;
-    }
+	@NonNull
+	public static String nonNullString(@Nullable String str) {
+		return str == null ? "" : str;
+	}
 
 }
